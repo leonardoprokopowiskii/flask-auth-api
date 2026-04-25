00d88dfe2096e33e123149fc697cfd551247c99f
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-04-24 22:06:05 -0300

    created route for read user

--- app.py
+++ app.py
@@ -53,5 +53,15 @@ def create_user():
 
     return jsonify({"message": "Dados inválidas!"}), 400
 
+@app.route('/user/<int:id_user>', methods=['GET'])
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
+
+    if user:
+        return jsonify({"username": user.username})
+    
+    return jsonify({"message": "Usuário não encontrado!"}), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
