e0dab0f8c8a74c96ad7c40930822ebe4b4f31a12
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-04-24 22:21:44 -0300

    created route for update user

--- app.py
+++ app.py
@@ -63,5 +63,18 @@ def read_user(id_user):
     
     return jsonify({"message": "Usuário não encontrado!"}), 404
 
+@app.route('/user/<int:id_user>', methods=['PUT'])
+@login_required
+def update_user(id_user):
+    data = request.json
+    user = User.query.get(id_user)
+
+    if user and data.get('password'):
+        user.password = data.get('password')
+        db.session.commit()
+        return jsonify({"message": f"Usuário {id_user} atualizado com sucesso"})
+    
+    return jsonify({"message": "Usuário não encontrado!"})
+
 if __name__ == "__main__":
     app.run(debug=True)
