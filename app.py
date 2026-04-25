8b0005a7f53462106e96056b77ff832077a19a86
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-04-24 21:17:04 -0300

    create logout route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -32,6 +32,12 @@ def login():
             return jsonify({"message": "Autenticação realizada com sucesso!"})
 
     return jsonify({"message": "Credenciais inválidas!"}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
 
 if __name__ == "__main__":
     app.run(debug=True)
